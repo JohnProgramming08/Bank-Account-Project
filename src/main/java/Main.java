e221e083b458ac4f81a755803640e3ff40778c11
JohnProgramming08 <157177709+JohnProgramming08@users.noreply.github.com>
2024-08-02 20:50:35 +0000

    Complete menu UI

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,13 +2,21 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[] args) {
-    
+    Scanner sc = new Scanner(System.in);
   }
 
-  public void display_menu(float balance) {
+  public static int display_menu(double balance, Scanner sc) {
+    // Output the menu and return their selection
     System.out.println("==========");
     System.out.println("MENU");
     System.out.println("==========");
     System.out.println("BALANCE: £" + balance);
+    System.out.println("(1)  WITHDRAW FUNDS");
+    System.out.println("(2)  DEPOSIT FUNDS");
+    System.out.println("(3)  TRANSACTION LIST");
+    System.out.println("(4)  EXIT PROGRAM");
+    System.out.println("==========");
+    System.out.print("Enter your selection: ");
+    return sc.nextInt();
   }
 }
