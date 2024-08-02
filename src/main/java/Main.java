7c39fcd0405fbd712a9e72233d9a4b3d3a0cf49e
JohnProgramming08 <157177709+JohnProgramming08@users.noreply.github.com>
2024-08-02 18:33:41 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    
+  }
+
+  public void display_menu(float balance) {
+    System.out.println("==========");
+    System.out.println("MENU");
+    System.out.println("==========");
+    System.out.println("BALANCE: £" + balance);
+  }
+}
